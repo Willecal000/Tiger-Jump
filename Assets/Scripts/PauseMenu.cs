0d1b58ecf2e1e54c18260450cee47a9528c27de6
MUKILTEO\1729521 <messy.caleb110@gmail.com>
2026-09-22 10:04:14 -0700

    added game menu, unfinished enemy mechanics

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    private bool isPaused;
+    [SerializeField] private GameObject _pauseMenu; // serializefield is so you can access the variable from the inspector but not from other scripts so its not public 100%
+    public void Pause()
+    {
+        isPaused = true;
+        _pauseMenu.SetActive(true);
+
+        Time.timeScale = 0;
+    }
+    public void Resume()
+    {
+        isPaused = false;
+        _pauseMenu.SetActive(false);
+
+        Time.timeScale = 1;
+    }
+    public void Toggle(InputAction.CallbackContext ctx)
+    {
+        isPaused = !isPaused;
+        _pauseMenu.SetActive(isPaused);
+
+        Time.timeScale = isPaused ? 0 : 1;
+    }
+    public void Quit()
+    {
+        SceneManager.LoadScene("GameMenu");
+    }
+}
